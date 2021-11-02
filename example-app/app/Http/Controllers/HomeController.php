6df86ecda7cc63b35c89ca3e2b1730f48d144c54
inhix <arseggg@gmail.com>
2021-11-02 18:29:53 +0200

    fourth commit

--- example-app/app/Http/Controllers/HomeController.php
+++ example-app/app/Http/Controllers/HomeController.php
@@ -3,32 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\People;
-use App\SWAPIPeopleResponse;
-use App\SWAPIPlanetResponse;
-use Psr\Log\NullLogger;
-
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $allHumans = People::getAllPeople();
 
-        $responce = new SWAPIPeopleResponse('https://swapi.dev/api/people/');
-
-        while ($responce->next !== null):
-            foreach ($responce->results as $human)
-                $allHumans [] = $human;
-            $responce = new SWAPIPeopleResponse($responce->next);
-        endwhile;
-
-        if($responce->next === null) {
-            foreach ($responce->results as $human)
-                $allHumans [] = $human;
-        }
-
-
-
-        return view('welcome', ['allHumans' => $allHumans, 'num' => 0]);
+        return view('table', ['allHumans' => $allHumans, 'num' => 0]);
     }
 
 }
